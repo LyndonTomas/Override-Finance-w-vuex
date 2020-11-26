86fa55aa6778b2031e88c37c1b61cdb46a78c312
LyndonTomas <201801180@iacademy.edu.ph>
2020-11-26 16:35:05 +0800

    deployable v1.2

--- server/index.js
+++ server/index.js
@@ -16,6 +16,13 @@ app.use('/api/users', users);
 const orders = require('./routes/api/orders');
 app.use('/api/orders', orders);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(__dirname + "/public/"));
+    app.get("*", (req, res) => {
+      res.sendFile(__dirname + "/public/index.html");
+    });
+  }
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
